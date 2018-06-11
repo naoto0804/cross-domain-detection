2e962e99cf77b22975ea67f3e62025dac59cd854
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2018-06-11 20:10:46 +0900

    update models

--- models/download.sh
+++ models/download.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-names=(clipart watercolor comic)
-for name in "${names[@]}"
-do
-    curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/cross_domain_detection/models/${name}_ssd300
-done
+curl -O http://www.hal.t.u-tokyo.ac.jp/~inoue/projects/cross_domain_detection/models/all.zip
+unzip all.zip
